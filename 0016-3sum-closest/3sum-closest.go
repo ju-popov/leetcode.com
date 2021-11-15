429ee60b4c560ebceb96d19ff4252a80cd1a4159
Julian Popov <julian.popov@sumup.com>
2021-11-15 06:38:28 +0200

    first commit

---
+++ 0016-3sum-closest/3sum-closest.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func calcResult(n1 int, n2 int, n3 int, target int) (int, int) {
+	total := n1 + n2 + n3
+	offset := target - total
+	if offset < 0 {
+		offset = -offset
+	}
+
+	return total, offset
+}
+
+func threeSumClosest(nums []int, target int) int {
+	sort.Ints(nums)
+
+	result, resultOffset := calcResult(nums[0], nums[1], nums[2], target)
+
+	for index := 0; index < len(nums)-2; index++ {
+		if (index != 0) && (nums[index] == nums[index-1]) {
+			continue
+		}
+
+		left := index + 1
+		right := len(nums) - 1
+
+		for left < right {
+			if (left != index+1) && (nums[left] == nums[left-1]) {
+				left++
+				continue
+			}
+
+			sum, sumOffset := calcResult(nums[index], nums[left], nums[right], target)
+			if sumOffset < resultOffset {
+				result, resultOffset = sum, sumOffset
+			}
+
+			if sum > target {
+				right--
+				continue
+			}
+
+			left++
+		}
+	}
+
+	return result
+}
+
+func main() {
+	// threeSumClosest([]int{-1, 2, 1, -4}, 1)
+	// fmt.Println(threeSumClosest([]int{-1, 2, 1, -4}, 1)) // 2
+	// fmt.Println(threeSumClosest([]int{0, 0, 0}, 1))      // 0
+	fmt.Println(threeSumClosest([]int{-100, -98, -2, -1}, -101)) // -101
+}
