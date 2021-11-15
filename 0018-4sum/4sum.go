f67fb605b611902d0067ecb3c56a59c792554d75
Julian Popov <julian.popov@sumup.com>
2021-11-15 09:36:33 +0200

    first commit

---
+++ 0018-4sum/4sum.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func fourSum(nums []int, target int) [][]int {
+	result := make([][]int, 0)
+
+	sort.Ints(nums)
+
+	for index1 := 0; index1 < len(nums)-3; index1++ {
+		if (index1 != 0) && (nums[index1] == nums[index1-1]) {
+			continue
+		}
+
+		for index2 := index1 + 1; index2 < len(nums)-2; index2++ {
+			if (index2 != index1+1) && (nums[index2] == nums[index2-1]) {
+				continue
+			}
+
+			left := index2 + 1
+			right := len(nums) - 1
+
+			for left < right {
+				if (left != index2+1) && (nums[left] == nums[left-1]) {
+					left++
+					continue
+				}
+
+				sum := nums[index1] + nums[index2] + nums[left] + nums[right]
+
+				if sum == target {
+					result = append(result, []int{nums[index1], nums[index2], nums[left], nums[right]})
+				}
+
+				if sum > target {
+					right--
+					continue
+				}
+
+				left++
+			}
+		}
+	}
+
+	return result
+}
+
+func main() {
+	fmt.Println(fourSum([]int{1, 0, -1, 0, -2, 2}, 0))   // [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+	fmt.Println(fourSum([]int{2, 2, 2, 2, 2}, 8))        // [[2,2,2,2]]
+	fmt.Println(fourSum([]int{-1, 0, 1, 2, -1, -4}, -1)) // [[-4,0,1,2],[-1,-1,0,1]]
+}
