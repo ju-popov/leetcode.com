2d227a209a8421b0e915495c0adc674ed4d0c1f3
Julian Popov <julian.popov@sumup.com>
2021-11-16 07:24:47 +0200

    first commit

---
+++ 0021-merge-two-sorted-lists/merge-two-sorted-lists.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+// https://leetcode.com/problems/merge-two-sorted-lists/
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func (head *ListNode) String() string {
+	buf := bytes.NewBufferString("")
+
+	for current := head; current != nil; current = current.Next {
+		if current != head {
+			if _, err := fmt.Fprint(buf, " -> "); err != nil {
+				panic(err)
+			}
+		}
+
+		if _, err := fmt.Fprintf(buf, "%v", current.Val); err != nil {
+			panic(err)
+		}
+	}
+
+	return buf.String()
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	dummyNode := &ListNode{}
+	last := dummyNode
+
+	for (l1 != nil) && (l2 != nil) {
+		if l1.Val <= l2.Val {
+			last.Next = l1
+			last = l1
+			l1 = l1.Next
+		} else {
+			last.Next = l2
+			last = l2
+			l2 = l2.Next
+		}
+	}
+
+	if l1 != nil {
+		last.Next = l1
+	} else if l2 != nil {
+		last.Next = l2
+	}
+
+	return dummyNode.Next
+}
+
+func main() {
+	inputList1 := &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 2,
+			Next: &ListNode{
+				Val: 4,
+			},
+		},
+	}
+
+	inputList2 := &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 3,
+			Next: &ListNode{
+				Val: 4,
+			},
+		},
+	}
+
+	fmt.Println(mergeTwoLists(inputList1, inputList2))
+}
