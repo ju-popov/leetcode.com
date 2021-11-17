5357d8767854e1fff1e3584374fe2ee6e31c9f0e
Julian Popov <julian.popov@sumup.com>
2021-11-17 03:50:20 +0200

    first commit

--- 0014-longest-common-prefix/longest-common-prefix.go
+++ 0014-longest-common-prefix/longest-common-prefix.go
@@ -1,5 +1,25 @@
 package main
 
+/*
+
+14. Longest Common Prefix
+
+https://leetcode.com/problems/longest-common-prefix/
+
+Approach 1: Horizontal scanning
+
+Complexity Analysis
+
+Time complexity : O(S) , where S is the sum of all characters in all strings.
+
+In the worst case all n strings are the same. The algorithm compares the string
+S1 with the other strings [S2..SN]. There are S character comparisons, where S
+is the sum of all characters in the input array.
+
+Space complexity : O(1). We only used constant extra space.
+
+*/
+
 import "fmt"
 
 func minInt(values ...int) int {
@@ -14,12 +34,13 @@ func minInt(values ...int) int {
 	return result
 }
 
-func longestCommonPrefixHelper(str1 string, str2 string) int {
+func longestCommonPrefixPosition(str1 string, str2 string) int {
 	result := -1
 
 	for index := 0; index < minInt(len(str1), len(str2)); index++ {
 		if str1[index] == str2[index] {
 			result = index
+
 			continue
 		}
 
@@ -30,20 +51,23 @@ func longestCommonPrefixHelper(str1 string, str2 string) int {
 }
 
 func longestCommonPrefix(strs []string) string {
-	str1 := strs[0]
+	result := strs[0]
 
 	for index := 1; index < len(strs); index++ {
-		result := longestCommonPrefixHelper(str1, strs[index])
-		if result < 0 {
+		pos := longestCommonPrefixPosition(result, strs[index])
+		if pos < 0 {
 			return ""
 		}
-		str1 = str1[:result+1]
+
+		result = result[:pos+1]
 	}
 
-	return str1
+	return result
 }
 
 func main() {
-	fmt.Println(longestCommonPrefix([]string{"flower", "flow", "flight"})) // fl
-	fmt.Println(longestCommonPrefix([]string{"dog", "racecar", "car"}))    //
+	fmt.Println(longestCommonPrefix(
+		[]string{"flower", "flow", "flight"},
+	)) // fl
+	fmt.Println(longestCommonPrefix([]string{"dog", "racecar", "car"})) //
 }
