89164812dbdd933cde53c4c4ec9f579bc9086e10
Julian Popov <julian.popov@sumup.com>
2021-11-20 03:11:17 +0200

    first commit

---
+++ 0051-0100/0058-length-of-last-word/length-of-last-word.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+/*
+
+58. Length of Last Word
+
+https://leetcode.com/problems/length-of-last-word/
+
+*/
+
+func lengthOfLastWord(s string) int {
+	result := 0
+	wordFound := false
+
+	for index := len(s) - 1; index >= 0; index-- {
+		char := s[index]
+
+		if char == ' ' {
+			if !wordFound {
+				continue
+			}
+
+			return result
+		}
+
+		wordFound = true
+		result++
+	}
+
+	return result
+}
+
+func main() {
+	fmt.Println(lengthOfLastWord("Hello World"))                 // 5
+	fmt.Println(lengthOfLastWord("   fly me   to   the moon  ")) // 4
+	fmt.Println(lengthOfLastWord("luffy is still joyboy"))       // 6
+}
